581d3c8456d5815c08e51ab542afd6492b5fd910
ngocquang <ngocquang2000fpt@gmail.com>
2022-09-18 12:35:43 +0700

    firstcommit

---
+++ src/main/java/com/observationclass/ObservationClassApplication.java
@@ -0,0 +1,11 @@
+package com.observationclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObservationClassApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ObservationClassApplication.class, args);
+    }
+}
---
+++ src/test/java/com/observationclass/ObservationClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.observationclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObservationClassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
