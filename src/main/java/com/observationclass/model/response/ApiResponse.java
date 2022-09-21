40480c0f595cccf3708539ab18ec9f5682e55e63
ngocquang <ngocquang2000fpt@gmail.com>
2022-09-21 19:19:35 +0700

    add properties file

---
+++ src/main/java/com/observationclass/model/response/ApiResponse.java
@@ -0,0 +1,15 @@
+package com.observationclass.model.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class ApiResponse {
+    private String status ;
+    private String message;
+    private Object items;
+}
